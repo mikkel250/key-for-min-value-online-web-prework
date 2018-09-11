002f4e719bac50d93997a077356140b3c938ca60
Mikkel Ridley <mikkel250@gmail.com>
2018-09-11 19:00:05 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,21 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+#use the block with a counter? Or spaceship operator? OR just collect into array and then compare them?
 def key_for_min_value(name_hash)
-
+  prev = Float::INFINITY
+  lowkey = "placeholder"
+  
+  return nil if name_hash.empty?
+  
+  name_hash.each do |k, v|
+  current = v
+  
+  lowkey = k if current < prev
+ 
+  prev = v if current < prev
+ 
+  end
+  lowkey
 end
+
